cae85c55d813f7fd4543c82a5940c222a0abdaa6
reamaestas <13636707-reamaestas@users.noreply.replit.com>
2023-08-31 03:34:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print('Hello, Python.')
+print(2001)
+print("What","do","commas","do?")
+print("Does", "adding",      "space", "matter?")
+print('Launch' + 'Code')
+print("LaunchCode \n was founded in", 2013)
+#forward slash n for new line
+print("----------")
+print("GUI - graphic user interface is a system designed with icons and visual representations of the machines files")
+print("--------------")
+print("CLI - command line interface uses textual commands to give computer instructions. apps running the CLI are terminals adn the program interpreting the commands are call the shell")
+print("the absolute path is the path to a file from the root directory \n and the relative path is the path to a file from the current directory")
+print("-----------")
+
